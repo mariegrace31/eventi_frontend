0a5439259356140d55ec856a9dbb1e7bb2ab41c0
mariegrace <mariegracebmg@gmail.com>
2024-03-07 18:55:25 +0200

    Add the navbar links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { PiSignIn } from "react-icons/pi";
+import { FaRegUser } from "react-icons/fa";
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <p className='logo'>EVENTI</p>
+      <div className='links'>
+        <a href="#events">Events</a>
+        <a href="#tickets">My tickets</a>
+      </div>
+      <div className='user'>
+        <div className="sign">
+        <a href="#signin">Sign in</a>
+        <PiSignIn className='sign-icon' />
+        </div>
+        <div className="prof">
+        <a href="profile">My Profile</a>
+        <FaRegUser className='prof-icon'/>
+        </div>
+      </div>
+    </nav>
   )
 }
 
